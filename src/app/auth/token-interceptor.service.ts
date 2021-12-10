bc52f1e69103e985b6a6b42c80dafb0fd94634b6
Akash Sinha <Akash_Sinha1@dell.com>
2021-12-10 11:56:00 +0530

    added interceptors

---
+++ src/app/auth/token-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private auth: AuthService) { }
+
+  // tslint:disable-next-line: no-any
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.auth.getToken()}`
+      }
+    });
+
+    return next.handle(req);
+  }
+}
